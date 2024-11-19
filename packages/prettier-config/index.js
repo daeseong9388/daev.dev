9c83560464a8cbc3add0798f93970962aaf55954
Jeon Daeseong <ds.jeon9388@gmail.com>
2024-11-19 12:34:37 +0900

    style: format prettier config file for improved readability

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -3,39 +3,37 @@
 
 /** @type { PrettierConfig | SortImportsConfig } */
 const config = {
-    plugins: [
-        "@ianvs/prettier-plugin-sort-imports",
-    ],
-    importOrder: [
-        "<TYPES>",
-        "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-        "^(next/(.*)$)|^(next$)",
-        "<THIRD_PARTY_MODULES>",
-        "",
-        "<TYPES>^@acme",
-        "^@acme/(.*)$",
-        "",
-        "<TYPES>^[.|..|~]",
-        "^~/",
-        "^[../]",
-        "^[./]",
-    ],
-    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-    importOrderTypeScriptVersion: "4.4.0",
-    overrides: [
-        {
-            files: "*.json.hbs",
-            options: {
-                parser: "json",
-            },
-        },
-        {
-            files: "*.js.hbs",
-            options: {
-                parser: "babel",
-            },
-        },
-    ],
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<TYPES>",
+    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "<TYPES>^@acme",
+    "^@acme/(.*)$",
+    "",
+    "<TYPES>^[.|..|~]",
+    "^~/",
+    "^[../]",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
+  ],
 };
 
 export default config;
